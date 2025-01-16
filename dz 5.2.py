a4cffad3047e67466c314c627a5943c78a095306
Sokolov53421 <isokolov55530@gmail.com>
2025-01-17 01:57:27 +0200

    modified calculator

---
+++ dz 5.2.py
@@ -0,0 +1,27 @@
+while True:
+    num1 = float(input("first number"))
+    num2 = float(input("second number"))
+    action = input("please enter following characters '+,-,/,*")
+
+    if action in "+":
+        result = num1 + num2
+    elif action in "-":
+        result = num1 - num2
+    elif action in "*":
+        result = num1 * num2
+    elif action == "/":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            print("Error")
+            result = None
+
+    print(result)
+
+    restart = input("To continue, type 'yes'.").lower()
+    if restart.lower() == "yes" :
+        continue
+
+
+    else:
+        break

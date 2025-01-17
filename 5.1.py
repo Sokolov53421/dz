2093b63b3a33a9a4836a98ec27c3b5c3e21a6220
Sokolov53421 <isokolov55530@gmail.com>
2025-01-18 01:14:50 +0200

    modified calculator #

---
+++ 5.1.py
@@ -0,0 +1,15 @@
+import string
+import keyword
+
+input_string = input("Enter name: ")
+
+if input_string[0].isdigit():
+    print(False)
+elif any(char in string.punctuation and char != "_" or char.isupper() for char in input_string):
+    print(False)
+elif input_string in keyword.kwlist:
+    print(False)
+elif input_string.strip("_") == "":
+    print(False)
+else:
+    print(True)

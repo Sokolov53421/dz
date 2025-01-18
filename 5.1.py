9d315c8269e5801199aabced91d5f3a1e5b57087
Sokolov53421 <isokolov55530@gmail.com>
2025-01-18 22:36:59 +0200

    modified calculator # 🤬

--- 5.1.py
+++ 5.1.py
@@ -3,13 +3,19 @@ import keyword
 
 input_string = input("Enter name: ")
 
-if input_string[0].isdigit():
+if " " in input_string:
+    print(False)
+elif input_string[0].isdigit():
     print(False)
 elif any(char in string.punctuation and char != "_" or char.isupper() for char in input_string):
     print(False)
 elif input_string in keyword.kwlist:
     print(False)
-elif input_string.count("_")>1:
+elif input_string == "__":
+    print(False)
+elif input_string.startswith("__")and input_string.count("_")>2:
+    print(False)
+elif input_string.count("_")>1 and not input_string.startswith("__"):
     print(False)
 else:
     print(True)

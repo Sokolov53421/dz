cc3069c0071316cea67d4acb699e8c9cea085bb6
Sokolov53421 <isokolov55530@gmail.com>
2025-01-22 01:58:02 +0200

    modified calculator # 🤬 a-c 🕛 999

--- 6.2.py
+++ 6.2.py
@@ -7,10 +7,14 @@ if 0 <= seconds < 8640000:
     result_seconds = seconds % 60
     result_minutes = minutes % 60
     result_hours = hours % 24
-    if days > 1:
-        days_word = "days"
+    if 11 <= days %100 <= 19:
+        days_word = "днів"
+    elif days %10 == 1:
+        days_word="день"
+    elif 2<=days % 10 <=4:
+        days_word="дні"
     else:
-        days_word = "day"
+        days_word = "днів"
 
     print(f"{days} {days_word} {result_hours:02d}:{result_minutes:02d}:{result_seconds:02d}")
 

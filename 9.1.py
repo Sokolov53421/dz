7a7bd4d1693d9dacf23ac994d73b0ef322955656
Sokolov53421 <isokolov55530@gmail.com>
2025-02-02 01:59:55 +0200

    modified calculator # 🤬 a-c 🕛 999

---
+++ 9.1.py
@@ -0,0 +1,12 @@
+def popular_words(text, words):
+    text = text.lower()
+    text_words = text.split()
+    result = {}
+    for word in words:
+        result[word] = text_words.count(word)
+
+    return result
+
+
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+print('OK')

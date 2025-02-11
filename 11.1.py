12fe974c6910e58eef630ba75c19fc352b53fa42
Sokolov53421 <isokolov55530@gmail.com>
2025-02-11 23:09:28 +0200

    modified calculator # 🤬 a-c 🕛 999

---
+++ 11.1.py
@@ -0,0 +1,22 @@
+import types
+
+
+def prime_generator(limit):
+    for num in range(2, limit + 1):
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            yield num
+
+
+gen = prime_generator(10)
+
+assert isinstance(gen, types.GeneratorType) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+
+print('Ok')
